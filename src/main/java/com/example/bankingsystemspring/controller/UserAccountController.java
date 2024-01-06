993a8b502559ea352341b018e0a87e1f1e912eab
LopesZ3R4 <jl.moreno7@hotmail.com>
2024-01-06 19:38:59 -0400

    added properties validation, withdraw and deposit endpoint

--- src/main/java/com/example/bankingsystemspring/controller/UserAccountController.java
+++ src/main/java/com/example/bankingsystemspring/controller/UserAccountController.java
@@ -2,9 +2,18 @@ package com.example.bankingsystemspring.controller;
 
 import com.example.bankingsystemspring.model.UserAccountEntity;
 import com.example.bankingsystemspring.service.UserAccountService;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.UUID;
+@Validated
 @RestController
 @RequestMapping("/api/useraccount")
 public class UserAccountController {
@@ -19,5 +28,38 @@ public class UserAccountController {
     @PostMapping
     public UserAccountEntity createUserAccount(@RequestBody UserAccountEntity userAccount) {
         return userAccountService.createUserAccount(userAccount);
+    }
+    @PostMapping(value = "deposit/{accountId}")
+    public ResponseEntity<Object> deposit(@PathVariable @NotNull UUID accountId,@RequestBody @NotNull @Min(1) BigDecimal amount) {
+        Optional<UserAccountEntity> userAccount = userAccountService.findById(accountId);
+
+        if (userAccount.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        UserAccountEntity updatedUserAccount = userAccountService.deposit(userAccount.get(),amount);
+
+        if (updatedUserAccount == null) {
+            return new ResponseEntity<>("Withdrawal failed due to insufficient balance", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(updatedUserAccount, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "withdraw/{accountId}")
+    public ResponseEntity<Object> withdraw(@PathVariable @NotNull UUID accountId, @RequestBody @NotNull @Min(1) BigDecimal amount) {
+
+        Optional<UserAccountEntity> userAccount = userAccountService.findById(accountId);
+
+        if (userAccount.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        UserAccountEntity updatedUserAccount = userAccountService.withdraw(userAccount.get(),amount);
+
+        if (updatedUserAccount == null) {
+            return new ResponseEntity<>("Withdrawal failed due to insufficient balance", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(updatedUserAccount, HttpStatus.OK);
     }
 }
