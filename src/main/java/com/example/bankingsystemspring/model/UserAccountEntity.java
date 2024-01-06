20cb740c085e6f07b5d666448e556cf8a3486652
LopesZ3R4 <jl.moreno7@hotmail.com>
2024-01-06 14:01:00 -0400

    changes on account propertiesw

--- src/main/java/com/example/bankingsystemspring/model/UserAccountEntity.java
+++ src/main/java/com/example/bankingsystemspring/model/UserAccountEntity.java
@@ -4,15 +4,18 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.UUID;
 
 @Entity
-@Table(name = "Account")
+@Table(name = "Accounts")
 public class UserAccountEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "AccountID")
-    private Long accountId;
+    private UUID accountId;
+    @Column(name = "ChavePix", nullable = false)
+    private String chavePix;
 
     @Column(name = "AccountNumber", unique = true, nullable = false)
     private String accountNumber;
@@ -29,5 +32,56 @@ public class UserAccountEntity {
     @Column(name = "UpdatedAt")
     private Timestamp updatedAt;
 
-    // Getters and Setters
+    public UUID getAccountId() {
+        return accountId;
+    }
+
+    public UserAccountEntity setAccountId(UUID accountId) {
+        this.accountId = accountId;
+        return this;
+    }
+
+    public String getChavePix() {
+        return chavePix;
+    }
+
+    public UserAccountEntity setChavePix(String chavePix) {
+        this.chavePix = chavePix;
+        return this;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public UserAccountEntity setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+        return this;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public UserAccountEntity setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+        return this;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public UserAccountEntity setBalance(BigDecimal balance) {
+        this.balance = balance;
+        return this;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
 }
--- src/main/java/com/example/bankingsystemspring/service/UserAccountService.java
+++ src/main/java/com/example/bankingsystemspring/service/UserAccountService.java
@@ -1,8 +1,11 @@
 package com.example.bankingsystemspring.service;
 
 import com.example.bankingsystemspring.model.UserAccountEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserAccountService {
-    public UserAccountEntity createUserAccount(UserAccountEntity userAccount) {
-        return null;
-    }
+   public UserAccountEntity createUserAccount(UserAccountEntity userAccount) {
+       return null;
+   }
 }
