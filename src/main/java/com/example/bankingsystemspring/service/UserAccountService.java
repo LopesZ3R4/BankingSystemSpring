666a9f301bd051608c0b6755844537c7f7071ac6
LopesZ3R4 <jl.moreno7@hotmail.com>
2024-01-07 14:41:24 -0400

    new method to handle transactions

--- src/main/java/com/example/bankingsystemspring/service/UserAccountService.java
+++ src/main/java/com/example/bankingsystemspring/service/UserAccountService.java
@@ -1,7 +1,9 @@
 package com.example.bankingsystemspring.service;
 
+import com.example.bankingsystemspring.common.TransactionType;
 import com.example.bankingsystemspring.model.UserAccountEntity;
 import com.example.bankingsystemspring.repository.UserAccountRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -34,6 +36,28 @@ public class UserAccountService {
     public UserAccountEntity deposit(UserAccountEntity userAccount, BigDecimal amount) {
         userAccount.setBalance(userAccount.getBalance().add(amount));
         return userAccountRepository.save(userAccount);
+    }
+
+    @Transactional
+    public void processTransaction(UserAccountEntity originAccount, UserAccountEntity destinationAccount, TransactionType transactionType, BigDecimal amount){
+        switch (transactionType) {
+            case deposit:
+                originAccount.setBalance(originAccount.getBalance().add(amount));
+                userAccountRepository.save(originAccount);
+                return;
+            case withdraw:
+                originAccount.setBalance(originAccount.getBalance().subtract(amount));
+                userAccountRepository.save(originAccount);
+                return;
+            case transfer:
+                originAccount.setBalance(originAccount.getBalance().subtract(amount));
+                destinationAccount.setBalance(destinationAccount.getBalance().add(amount));
+                userAccountRepository.save(destinationAccount);
+                userAccountRepository.save(originAccount);
+                return;
+            default:
+                throw new IllegalArgumentException("Invalid transaction type");
+        }
     }
 
     public Optional<UserAccountEntity> findById(UUID accountId) {
