c71e44ffafcbb9056344f8513516ae411320cfad
joao.moreno <jl.moreno7@hotmail.com>
2024-01-10 14:13:08 -0400

    calculateBalance refactor

--- src/main/java/com/example/bankingsystemspring/service/UserAccountService.java
+++ src/main/java/com/example/bankingsystemspring/service/UserAccountService.java
@@ -1,7 +1,6 @@
 package com.example.bankingsystemspring.service;
 
 import com.example.bankingsystemspring.common.enums.TransactionType;
-import com.example.bankingsystemspring.model.AccountTransactionsEntity;
 import com.example.bankingsystemspring.model.UserAccountEntity;
 import com.example.bankingsystemspring.repository.AccountTransactionRepository;
 import com.example.bankingsystemspring.repository.UserAccountRepository;
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -37,15 +35,10 @@ public class UserAccountService {
         return userAccountRepository.findByChavePix(chavePix);
     }
     public BigDecimal calculateBalance(UserAccountEntity account) {
-        List<AccountTransactionsEntity> transactions = transactionRepository.findByAccount(account);
-        BigDecimal balance = BigDecimal.ZERO;
-        for (AccountTransactionsEntity transaction : transactions) {
-            if (transaction.getTransactionType() == TransactionType.deposit) {
-                balance = balance.add(transaction.getAmount());
-            } else if (transaction.getTransactionType() == TransactionType.withdraw) {
-                balance = balance.subtract(transaction.getAmount());
-            }
-        }
-        return balance;
+        return transactionRepository.findByAccount(account).stream()
+                .map(transaction -> transaction.getTransactionType() == TransactionType.deposit
+                        ? transaction.getAmount()
+                        : transaction.getAmount().negate())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
